c3cd435226977d69838e76b2f27fb614ad05e6c8
0X01 <charger_ciders_0t@icloud.com>
2025-12-26 13:35:38 -0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Download } from "lucide-react";
 import html2canvas from "html2canvas";
 import "./app.css";
@@ -9,37 +9,298 @@ const App = () => {
   const gridRef = useRef(null);
 
   const bingoItems = [
-    { text: "Venomous Jeweler" },
-    { text: "Catch These Hands" },
-    { text: "Demonic Arsenal" },
-    { text: "Eternal Love" },
-    { text: "Triple Double" },
-    { text: "Ranged Fortification" },
-    { text: "Melee Fortification" },
-    { text: "What Was That?" },
-    { text: "The Lizard Sticker" },
-    { text: "Theatre of Blood" },
-    { text: "Hellhound's Heels" },
-    { text: "Lord of the Bling" },
-    { text: "Risk Assessment" },
-    { text: "Lesser of Two Evils" },
-    { text: "Chambers of Xeric" },
-    { text: "Toxic Relationship" },
-    { text: "Who Sharded?" },
-    { text: "Mid-Game Magician" },
-    { text: "Elemental Elegance" },
-    { text: "Tombs of Amascut" },
-    { text: "Weaponized Moontism" },
-    { text: "Phantom Dancer" },
-    { text: "Gryphon Tamer" },
-    { text: "Skills Master" },
-    { text: "Bob Ross" },
-    { text: "Brothers Brawl" },
-    { text: "King of the Rats" },
-    { text: "Freaky Forester" },
-    { text: "Medieval Meleer" },
-    { text: "FeelsBadMan" },
-  ];
+  {
+    title: "Venomous Jeweler",
+    text: (
+      <ul>
+        <li>Araxyte fang</li>
+        <li>Onyx</li>
+        <li>Zenyte shard</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Catch These Hands",
+    text: (
+      <ul>
+        <li>Mokhaiotl cloth</li>
+        <li>Onyx</li>
+        <li>Zenyte shard</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Demonic Arsenal",
+    text: (
+      <ul>
+        <li>Any abyssal weapon</li>
+        <li>Tormented synapse</li>
+        <li>Eye of ayak</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Eternal Love",
+    text: (
+      <ul>
+        <li>Imbued heart</li>
+        <li className="or">OR</li>
+        <li>Eternal gem</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Triple Double",
+    text: (
+      <ul>
+        <li>Tonalztics of ralos</li>
+        <li>Dark bow</li>
+        <li>Fire element staff crown</li>
+        <li>Ice element staff crown</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Ranged Fortification",
+    text: (
+      <ul>
+        <li>Masori armor piece</li>
+        <li>Armadyl armor piece</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Melee Fortification",
+    text: (
+      <ul>
+        <li>Torva armor piece</li>
+        <li>Bandos armor piece (excluding Bandos boots)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "What Was That?",
+    text: (
+      <ul>
+        <li>Bandos boots</li>
+        <li>Black tourmaline core</li>
+        <li>Echo crystal</li>
+      </ul>
+    ),
+  },
+  {
+    title: "The Lizard Sticker",
+    text: (
+      <ul>
+        <li>Zamorakian spear</li>
+        <li>Hydra's claw</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Theatre of Blood",
+    text: (
+      <ul>
+        <li>2 purples from Theatre of Blood</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Hellhound's Heels",
+    text: (
+      <ul>
+        <li>Pegasian crystal + Ranger boots</li>
+        <li className="or">OR</li>
+        <li>Eternal crystal + Infinity boots</li>
+        <li className="or">OR</li>
+        <li>Primordial crystal + Dragon boots</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Lord of the Bling",
+    text: (
+      <ul>
+        <li>DT2 Vestige</li>
+        <li>Matching DK ring</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Risk Assessment",
+    text: (
+      <ul>
+        <li>Craw's bow + Fangs of venenatis</li>
+        <li className="or">OR</li>
+        <li>Viggora's chainmace + Claws of callisto</li>
+        <li className="or">OR</li>
+        <li>Thammaron's sceptre + Skull of vet'ion</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Lesser of Two Evils",
+    text: (
+      <ul>
+        <li>Nightmare unique</li>
+        <li className="or">OR</li>
+        <li>Yama unique</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Chambers of Xeric",
+    text: (
+      <ul>
+        <li>2 purples from Chambers of Xeric</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Toxic Relationship",
+    text: (
+      <ul>
+        <li>Staff of the dead</li>
+        <li>Magic fang</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Who Sharded?",
+    text: (
+      <ul>
+        <li>Any Godsword hilt</li>
+        <li>3 unique Godsword shards</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Plunderin' Booty",
+    text: (
+      <ul>
+        <li>Tiny pearl</li>
+        <li>Ray barbs</li>
+        <li>Swift albatross feather</li>
+        <li>Echo pearl</li>
+        <li>Narwhal horn</li>
+        <li>Dragon metal sheet</li>
+        <li>Dragon nails</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Elemental Elegance",
+    text: (
+      <ul>
+        <li>Tome of fire</li>
+        <li>Tome of water</li>
+        <li>Tome of earth</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Tombs of Amascut",
+    text: (
+      <ul>
+        <li>2 purples from Tombs of Amascut</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Weaponized Moontism",
+    text: (
+      <ul>
+        <li>Dual macuahuitl</li>
+        <li>Eclipse atlatl</li>
+        <li>Blue moon spear</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Phantom Dancer",
+    text: (
+      <ul>
+        <li>3 Venator shards</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Gryphon Tamer",
+    text: (
+      <ul>
+        <li>Belle's folly</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Skills Master",
+    text: (
+      <ul>
+        <li>Choose 3:</li>
+        <li>Fletching knife</li>
+        <li>Tackle box</li>
+        <li>Fish barrel</li>
+        <li>Bruma torch</li>
+        <li>Strange old lockpick</li>
+        <li>Imcando hammer</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Bob Ross",
+    text: (
+      <ul>
+        <li>3 tradeable boat paints (excluding Merchant's paint)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Brothers Brawl",
+    text: (
+      <ul>
+        <li>Full set of Barrows armor (any helm/chest/legs)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "King of the Rats",
+    text: (
+      <ul>
+        <li>Brine sabre</li>
+        <li>3 Scurrius' spines</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Freaky Forester",
+    text: (
+      <ul>
+        <li>Golden pheasant egg</li>
+        <li>Fox whistle</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Medieval Meleer",
+    text: (
+      <ul>
+        <li>Sulphur blades</li>
+        <li>Glacial temotli</li>
+        <li>Earthbound tecpatl</li>
+      </ul>
+    ),
+  },
+  {
+    title: "FeelsBadMan",
+    text: (
+      <ul>
+        <li>Champion scroll</li>
+        <li>Curved bone</li>
+        <li>Long bone</li>
+      </ul>
+    ),
+  },
+];
 
   const toggleSquare = (index) => {
     setClickedSquares((prev) => {
@@ -74,6 +335,8 @@ const App = () => {
       alert("Failed to save image. Please try again.");
     }
   };
+
+
 
   return (
     <div className="min-h-screen p-8" style={{ backgroundColor: "#2E2C29" }}>
@@ -113,7 +376,7 @@ const App = () => {
                   style={{
                     // backgroundColor: "#46433A",
                     borderWidth: "2px",
-                    borderColor: isClicked ? "#E6A519" : "#474745",
+                    borderColor: isClicked ? "#E6A519" : "transparent",
                     borderStyle: "solid",
                   }}
                   onClick={() => toggleSquare(index)}
@@ -126,7 +389,7 @@ const App = () => {
                   onMouseLeave={(e) => {
                     setHoveredSquare(null);
                     if (!isClicked) {
-                      e.currentTarget.style.borderColor = "#474745";
+                      e.currentTarget.style.borderColor = "transparent";
                     }
                   }}
                 >
@@ -163,9 +426,21 @@ const App = () => {
             {hoveredSquare !== null
               ? (
                 <div className="text-center">
-                  <p
+                  <h3
                     className="text-3xl font-semibold"
-                    style={{ color: "#FFCF3F" }}
+                    style={{
+                      color: "#FFCF3F",
+                      textShadow: "2px 2px 0px black",
+                    }}
+                  >
+                    {bingoItems[hoveredSquare].title}
+                  </h3>
+                  <p
+                    className="text-xl mt-2"
+                    style={{
+                      color: "#FFCF3F",
+                      textShadow: "2px 2px 0px black",
+                    }}
                   >
                     {bingoItems[hoveredSquare].text}
                   </p>
@@ -188,7 +463,7 @@ const App = () => {
             style={{
               backgroundColor: "#E6A519",
               color: "#0F0F0F",
-              cursor:'pointer'
+              cursor: "pointer",
             }}
             onMouseEnter={(e) =>
               e.target.style.backgroundColor = "#FFCF3F"}
